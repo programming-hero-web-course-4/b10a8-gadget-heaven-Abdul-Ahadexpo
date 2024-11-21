67783cf8f1266289a7226ff40d2a62ca8d703647
Abdul-Ahadexpo <adahadd4845e@gmail.com>
2024-11-21 23:52:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+      <div className="min-h-[calc(100vh-288px)] py-12">
+        {/* Dynamic Section */}
+        <Outlet />
+      </div>
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
